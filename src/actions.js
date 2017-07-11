2c7057ef83d4fb1f5bac9450ac40c66b907ae137
yogieputra8 <yogie.putra8@gmail.com>
2017-07-11 12:58:17 +0700

    add redux observable with rxjs

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,22 @@
-import { FETCHING_DATA } from './constants'
+import { FETCHING_DATA, FETCHING_DATA_SUCCESS, FETCHING_DATA_FAILURE } from './constants'
+
 
 export function fetchData() {
-  return {
-    type: FETCHING_DATA
-  }
+	return {
+		type: FETCHING_DATA
+	}
+}
+
+export function getDataSuccess(data){
+	return {
+		type: FETCHING_DATA_SUCCESS,
+		data
+	}
+}
+
+export function getDataFailure(error){
+	return {
+		type: FETCHING_DATA_FAILURE,
+		errorMessage: error
+	}
 }
